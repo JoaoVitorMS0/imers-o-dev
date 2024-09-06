e195a4375f20e17e17934e57f12149c8e36c6470
João Vitor <jvmendonca10@hotmail.com>
2024-09-06 12:09:44 -0300

    Atualização no app.js
    Adicionado o filtro no botão busca. Quando tenta pesquisar nada ou algo que não tem nos dados mostra um erro.

--- app.js
+++ app.js
@@ -2,14 +2,33 @@ function pesquisar() {
   // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
   let section = document.getElementById("resultados-pesquisa");
 
+  let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+  // se campoPesquisa for uma string sem nada
+  if (!campoPesquisa) {
+    section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome do jogador ou esporte!</p>"
+    return 
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase()
+
+
   // Inicializa uma string vazia para armazenar os resultados da pesquisa
-  let result = "";
+  let resultados = "";
+  let titulo = "";
+  let descricao = "";
+  let tags = "";
 
   // Itera sobre cada dado na lista de dados
   for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags = dado.tags.toLocaleLowerCase()
+    // Se titulo includess campoPesquisa
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
     // Constrói o HTML para cada item do resultado da pesquisa,
     // utilizando template literals para formatar a string
-    result += `
+      resultados += `
       <div class="item-resultado">
         <h2>
           <a href=${dado.link} target="_blank">${dado.titulo}</a>
@@ -18,8 +37,15 @@ function pesquisar() {
         <a href=${dado.link} target="_blank">Mais informações</a>
       </div>
     `;
+    }
+    
+    
+  }
+
+  if (!resultados) {
+    resultados = "<p>Nada foi encontrado</p>"
   }
 
   // Atribui o HTML gerado à seção de resultados
-  section.innerHTML = result;
+  section.innerHTML = resultados;
 }
