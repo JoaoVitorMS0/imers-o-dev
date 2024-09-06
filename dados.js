206ddffb04c90c6f774e15cf6377b2f9dce9bd91
João Vitor <jvmendonca10@hotmail.com>
2024-09-06 12:10:15 -0300

    Atualização dados.js
    Adicionado "tags" para facilitar a busca

--- dados.js
+++ dados.js
@@ -1,28 +1,32 @@
 let dados = [
   {
-    titulo: 'FalleN',
+    titulo: 'Fallen',
     descricao: 'Fallen, o lendário capitão brasileiro de Counter-Strike: Global Offensive, é conhecido por sua liderança estratégica e habilidade de clutch. Com uma carreira marcada por títulos internacionais e recordes históricos, ele se tornou um ícone do cenário competitivo do jogo.',
-    link: 'https://pt.wikipedia.org/wiki/FalleN'
+    link: 'https://pt.wikipedia.org/wiki/FalleN',
+    tags: 'Jogadores, CS:GO, Brasil, jogador, liderança, estratégia, estrategia, Counter, Cs'
   },
   {
     titulo: 'Ninja',
     descricao: 'Ninja, também conhecido como Tyler Blevins, é um dos maiores streamers e jogadores profissionais de Fortnite. Conhecido por sua habilidade e carisma, ele popularizou o jogo e se tornou um ícone dos e-sports.',
-    link: 'https://en.wikipedia.org/wiki/Tyler_Blevins'
+    link: 'https://en.wikipedia.org/wiki/Tyler_Blevins',
+    tags: 'Jogadores, Fortnite, Estados Unidos, jogador, streamer, carisma'
   },
   {
     titulo: 'Faker',
     descricao: 'Faker, ou Lee Sang-hyeok, é considerado um dos maiores jogadores de League of Legends de todos os tempos. Sua mecânica impecável e jogo estratégico o tornaram uma lenda no MOBA da Riot Games.',
-    link: 'https://pt.wikipedia.org/wiki/Lee_Sang-hyeok'
+    link: 'https://pt.wikipedia.org/wiki/Lee_Sang-hyeok',
+    tags: 'Jogadores, LoL, Coreia do Sul, jogador, estratégia, mecânica, estrategia, Lol'
   },
   {
     titulo: 'Shroud',
     descricao: 'Shroud, ou Michael Grzesiek, é um ex-jogador profissional de Counter-Strike: Global Offensive, conhecido por suas habilidades de aim e streams de alta qualidade. Atualmente, ele é um dos streamers mais populares do Twitch.',
-    link: 'https://en.wikipedia.org/wiki/Michael_Grzesiek'
+    link: 'https://en.wikipedia.org/wiki/Michael_Grzesiek',
+    tags: 'Jogadores, CS:GO, Estados Unidos, jogador, streamer, aim, mira, Counter, Cs'
   },
   {
     titulo: 'Gaules',
     descricao: 'Gaules, ou Alexandre Chiqueta, é um dos maiores streamers e casters de Counter-Strike: Global Offensive do Brasil. Conhecido por sua paixão pelo jogo e pela comunidade, ele é uma figura central nos e-sports brasileiros.',
-    link: 'https://www.twitch.tv/gaules'
+    link: 'https://www.twitch.tv/gaules',
+    tags: 'Jogadores, CS:GO, Brasil, streamer, caster, comunidade, Counter, Cs'
   }
 ];
-
